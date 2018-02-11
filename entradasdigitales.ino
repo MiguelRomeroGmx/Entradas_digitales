4f35d928d03b180df8672a744dae7f51bdcf0cdf
MiguelRomero <contacto@miguelromero.mx>
2018-02-11 11:44:10 -0600

    Definicion de pines de entrada

--- entradasdigitales.ino
+++ entradasdigitales.ino
@@ -3,10 +3,18 @@
 
 void void setup()
 {
-	
+	pinMode(2, INPUT);  // definimos el pin 2 como entrada 
+	pinMode(3, INPUT);	// definimos el pin 3 como entrada 
+	pinMode(4, INPUT);	// definimos el pin 4 como entrada 
+	pinMode(5, OUTPUT);	// definimos el pin 5 como salida 
+	pinMode(6, OUTPUT);	// definimos el pin 6 como salida 
+	pinMode(7, OUTPUT);	// definimos el pin 7 como salida 
 }
 
 void void loop()
 {
+
+
+
 	
 }
