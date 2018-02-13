dbaef2d676e27f7f647d4efb2da1e9f94d3a259d
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-02-13 08:06:39 -0600

    variables

--- entradasdigitales.ino
+++ entradasdigitales.ino
@@ -1,6 +1,8 @@
 // Entradas Digitales
 // Las entradas digitales solo pueden recibir 2 valores, 0 y 1 (HIGH Y LOW)
 // Al igual que las salidas digitales solo pueden enviar 2 valores 0 y 1 
+// 
+int entrada1, entrada2, entrada3; // definición de variables para guardar los valores de las entradas
 
 void void setup()
 {
@@ -14,6 +16,11 @@ void void setup()
 
 void void loop()
 {
+
+entrada1 = digitalRead(2);  //Colocamos el valor de la entrada 2 en la variable entrada1
+entrada2 = digitalRead(3);	//...
+entrada3 = digitalRead(4);	//...
+
 
 
 
